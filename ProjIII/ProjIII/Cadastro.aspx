de7bf49acca58c3fd01eec3dff9381322bd2e9ba
Emmanuel <30930846+EmmanuelCasangel@users.noreply.github.com>
2018-09-04 11:57:49 -0300

    d

--- ProjIII/ProjIII/Cadastro.aspx
+++ ProjIII/ProjIII/Cadastro.aspx
@@ -18,12 +18,73 @@
 <body>
     <form id="form1" runat="server">
         
-        <div class="row">
-           <div class="input-field col s6">
-                <input placeholder="Placeholder" id="txtEmail" type="text" class="validate" runat="server">
-                <label for="txtEmail">Email</label>
+        <div class="container">
+            <div class="row">
+               <div class="input-field col s6">
+                    <input placeholder="Email" id="txtEmail" type="text" class="validate" runat="server">
+                    <label for="txtEmail">Email</label>
+                </div>
             </div>
+
+            <div class="row">
+               <div class="input-field col s6">
+                    <input placeholder="Nome" id="txtNome" type="text" class="validate" runat="server">
+                    <label for="txtNome">Nome</label>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="input-field col s6">
+                    <input placeholder="Senha" id="txtSenha" type="text" class="validate" runat="server">
+                    <label for="txtSenha">Senha</label>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="input-field col s4">
+                    <input placeholder="Cidade" id="txtCidade" type="text" class="validate" runat="server">
+                    <label for="txtCidade">Cidade</label>
+                </div>
+
+                <div class="input-field col s2">
+                    <input placeholder="Estado" id="txtEstado" type="text" class="validate" runat="server">
+                    <label for="txtEstado">Estado</label>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="input-field col s3">
+                    <input placeholder="Idade" id="txtIdade" type="text" class="validate" runat="server">
+                    <label for="txtIdade">Idade</label>
+                </div>
+
+                <div class="input-field col s3">
+                    <input placeholder="Celular" id="txtCelular" type="text" class="validate" runat="server">
+                    <label for="txtCelular">Celular</label>
+                </div>
+            </div>
+
+            
+            
+            <div class="row">
+                <div class="input-field col s3">
+                    <input placeholder="Funcao" id="txtFuncao" type="text" class="validate" runat="server">
+                    <label for="txtFuncao">Funcao</label>
+                </div>               
+            </div>
+
+            <div class="row">
+                <div class="input-field col s3">
+                    <input placeholder="Cod Cad" id="txtCodCad" type="text" class="validate" runat="server">
+                    <label for="txtCodCad">Codigo cadastro</label>
+                </div>       
+                &nbsp;<img src="imagens/usuario.jpg" height="180" width="180"  /></div>
+            
+            
+
+
         </div>
+
     </form>
 </body>
 </html>
--- ProjIII/ProjIII/Cadastro.aspx.designer.cs
+++ ProjIII/ProjIII/Cadastro.aspx.designer.cs
@@ -29,5 +29,77 @@ namespace ProjIII {
         /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlInputText txtEmail;
+        
+        /// <summary>
+        /// Controle txtNome.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText txtNome;
+        
+        /// <summary>
+        /// Controle txtSenha.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText txtSenha;
+        
+        /// <summary>
+        /// Controle txtCidade.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText txtCidade;
+        
+        /// <summary>
+        /// Controle txtEstado.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText txtEstado;
+        
+        /// <summary>
+        /// Controle txtIdade.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText txtIdade;
+        
+        /// <summary>
+        /// Controle txtCelular.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText txtCelular;
+        
+        /// <summary>
+        /// Controle txtFuncao.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText txtFuncao;
+        
+        /// <summary>
+        /// Controle txtCodCad.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText txtCodCad;
     }
 }
